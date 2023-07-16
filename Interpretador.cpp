7d05d3eb9e8b0b69b41d0b912e62d6dd41bf60ac
Marcio <marciobarros.2310@gmail.com>
2023-07-16 12:32:20 -0300

    colocando numa so estrutura funcao

--- Interpretador.cpp
+++ Interpretador.cpp
@@ -5,32 +5,43 @@
 using namespace std;
 
 
-struct No{
-    char operacao;
-    No *prox_dir;
-    No *prox_esq;
-
-    No(){
-        prox_dir = NULL;
-        prox_esq = NULL;
-    }
-};
-
 
 struct funcao{
 
+    struct No{
+        char operacao;
+        No *prox_dir;
+        No *prox_esq;
+
+        No(){
+            prox_dir = NULL;
+            prox_esq = NULL;
+        }
+    };
+
+
+    char *expressao;
     double *var;
-    int n;
+    No *raiz;
 
-    funcao(){
+    funcao(char *expr){
+        raiz = NULL;
+        expressao = expr;
         var = NULL;
-        n = 0;
     }
 
-    void vetorVar(int size){
-        var = (double*) malloc(size*sizeof(double));
-        n = size;
+    void vetorVar(double *vetor){
+        var = vetor;
     }
+
+    double valor(){
+        if(raiz == NULL){
+            raiz = (No*) malloc(sizeof(No));
+            interpretador(expressao,raiz);
+        }
+        return func(raiz);
+    }
+
 
     double func(No *no){
  
@@ -49,71 +60,61 @@ struct funcao{
 
         return var[stoi(&(no->operacao))];
     }
+
+    //vai interpretar e criar a arvore da expressao
+    char* interpretador(char *expr, No *no_anterior){
+        char *string_back_esq;
+        char *string_back_dir;
+        //lado esquerdo
+        if(expr[0] == '('){
+            //verificando se o caractere de agora associado ao no_anterior
+            //é o começo de uma expressão ou apenas uma base,
+            //caso seja, necessita dois filhos
+
+            no_anterior->prox_esq = (No*) malloc(sizeof(No));
+
+
+            char *prox_expr = &expr[1];
+            
+
+            string_back_esq = interpretador(prox_expr,no_anterior->prox_esq);
+            no_anterior->operacao = string_back_esq[0];
+            /*
+            indo pro lado direito da expressão
+            */
+
+            no_anterior->prox_dir = (No*) malloc(sizeof(No));
+
+            /*
+            voltou assim +b)+z) contudo pego apenas b)+z)
+            pois estou tirando a operação da propria recursao
+            */
+            char *string_dir = &string_back_esq[1];
+        
+            string_back_dir = interpretador(string_dir,no_anterior->prox_dir);
+
+
+            return &string_back_dir[1];
+
+        }else{
+            //caso base
+
+            no_anterior->operacao = expr[0];
+
+
+            return &expr[1];
+        }
+        
+    }
+
 };
 
-
-
-char* interpretador(char *expr, No *no_anterior){
-    char *string_back_esq;
-    char *string_back_dir;
-    //lado esquerdo
-    if(expr[0] == '('){
-        //verificando se o caractere de agora associado ao no_anterior
-        //é o começo de uma expressão ou apenas uma base,
-        //caso seja, necessita dois filhos
-
-        no_anterior->prox_esq = (No*) malloc(sizeof(No));
-
-
-        char *prox_expr = &expr[1];
-        
-
-        string_back_esq = interpretador(prox_expr,no_anterior->prox_esq);
-        no_anterior->operacao = string_back_esq[0];
-        /*
-        indo pro lado direito da expressão
-        */
-
-        no_anterior->prox_dir = (No*) malloc(sizeof(No));;
-
-        /*
-        voltou assim +b)+z) contudo pego apenas b)+z)
-        pois estou tirando a operação da propria recursao
-        */
-        char *string_dir = &string_back_esq[1];
-    
-        string_back_dir = interpretador(string_dir,no_anterior->prox_dir);
-
-
-        return &string_back_dir[1];
-
-    }else{
-        //caso base
-
-        no_anterior->operacao = expr[0];
-
-
-        return &expr[1];
-    }
-    
-}
-
-
-
 int main(){
+    double vetor[] = {2,3,4};
+    funcao func("((1-2)*0)");
+    func.vetorVar(vetor);
 
-    
-    char *expressao = "(0*(1+2))";
-    No raiz;
-    interpretador(expressao,&raiz);
-    funcao func;
-    func.vetorVar(2);
-    func.var[0] = 2;
-    func.var[1] = 1;
-    func.var[2] = 3;
-
-
-    cout<<"valor da expressao ="<<func.func(&raiz)<<"\n";
+    cout<<"valor da expressao ="<<func.valor()<<"\n";
     
     return 0;
 }
